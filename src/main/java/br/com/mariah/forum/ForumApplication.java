e55a66811e4f23262fcca108bc7fa38f73ba0b72
Matheus2021s <matheusmartinsdacosta21@gmail.com>
2021-10-03 22:46:18 -0300

    Enable cache to improve application performance incrasing response time

--- src/main/java/br/com/mariah/forum/ForumApplication.java
+++ src/main/java/br/com/mariah/forum/ForumApplication.java
@@ -2,10 +2,13 @@ package br.com.mariah.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
 
 @EnableSpringDataWebSupport
 @SpringBootApplication
+@EnableCaching
 public class ForumApplication {
 
 	public static void main(String[] args) {
@@ -13,3 +16,4 @@ public class ForumApplication {
 	}
 
 }
+ 
