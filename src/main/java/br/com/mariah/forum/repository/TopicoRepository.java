cd8846d9a3f81a98640670e311de0383513f0934
Matheus2021s <matheusmartinsdacosta21@gmail.com>
2021-10-03 22:32:40 -0300

    Topic list refactoring to simplify pagination

--- src/main/java/br/com/mariah/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/mariah/forum/repository/TopicoRepository.java
@@ -1,13 +1,13 @@
 package br.com.mariah.forum.repository;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.mariah.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long>{
 
-	Page<Topico> findByCursoNome(String nomeCurso, PageRequest paginacao);
+	Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 
 }
