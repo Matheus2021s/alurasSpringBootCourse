9e5b7f2aad15a5d7938276442752e634fc070498
Matheus2021s <matheusmartinsdacosta21@gmail.com>
2021-10-03 22:54:23 -0300

    Adding cache evict anotation in methods that alter register, delete, or create a new one.

--- src/main/java/br/com/mariah/forum/controller/TopicosController.java
+++ src/main/java/br/com/mariah/forum/controller/TopicosController.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -58,6 +59,7 @@ public class TopicosController {
 	
 	@PostMapping
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm topicoForm, UriComponentsBuilder builder) {
 		Topico topico = topicoForm.converter(cursoRepository);
 		this.topicoRepository.save(topico);
@@ -77,6 +79,7 @@ public class TopicosController {
 	
 	@PutMapping("/{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form ){
 		Optional<Topico> optional = this.topicoRepository.findById(id);
 		if (optional.isPresent()) {
@@ -89,6 +92,7 @@ public class TopicosController {
 	
 	@DeleteMapping("/{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<?> remover(@PathVariable Long id){
 		Optional<Topico> optional = this.topicoRepository.findById(id);
 		if (optional.isPresent()) {
