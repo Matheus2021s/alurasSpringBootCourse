058268eabe91feae934f8f833b54232f54ad65f8
Matheus2021s <matheusmartinsdacosta21@gmail.com>
2021-10-11 19:23:31 -0300

    Adding mock mvc to teste a request to controller

--- src/main/java/br/com/mariah/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/mariah/forum/config/security/SecurityConfigurations.java
@@ -19,7 +19,7 @@ import br.com.mariah.forum.repository.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
-@Profile("prod")
+@Profile(value = {"prod","test"})
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
 	@Autowired
