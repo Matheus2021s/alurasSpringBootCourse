c58ad8d46bcf417507f0602af967a6d5f0b71146
Matheus2021s <matheusmartinsdacosta21@gmail.com>
2021-10-11 18:50:39 -0300

    Adding another application.properties to connect another database instead of h2. Spring boot uses h2 database as a default database in tests. To use a diferent database, you have to force this using an specifc profile to that refers to a application.properties that have this datrabase connection configs

--- src/test/java/br/com/mariah/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/mariah/forum/repository/CursoRepositoryTest.java
@@ -4,11 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import br.com.mariah.forum.modelo.Curso;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ActiveProfiles("test")
 public class CursoRepositoryTest {
 
 	@Autowired
